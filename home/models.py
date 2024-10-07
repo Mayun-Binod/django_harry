a966bc59a199446b8722feaf33dc3e77b10540a9
Mayun-binod <shresthabinod334@gmail.com>
2024-10-08 04:19:42 +0545

    added

--- home/models.py
+++ home/models.py
@@ -7,3 +7,6 @@ class Contact(models.Model):
     phone=models.IntegerField()
     desc=models.TextField()
     date=models.DateField()
+
+    def __str__(self):
+        return f"{self.name}"
--- home/views.py
+++ home/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from datetime import datetime
+from home.models import Contact
+from django.contrib import messages
 
 # Create your views here.
 def index(request):
@@ -20,4 +23,12 @@ def services(request):
 
 def contact(request):
     # return HttpResponse('this is contact page')
+    if request.method=='POST':
+        name=request.POST.get('name')
+        email=request.POST.get('email')
+        phone=request.POST.get('phone')
+        desc=request.POST.get('desc')
+        contact=Contact(name=name,email=email,phone=phone,desc=desc,date=datetime.today())
+        contact.save()
+        messages.success(request, "Contact Form is Submitted !.")
     return render(request,'contact.html')
